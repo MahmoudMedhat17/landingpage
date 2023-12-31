3237235526f2b40c59d64bbce87185bc1d21c546
Mahmoud Medhat <M7moooud.17@gmail.com>
2024-01-01 00:36:15 +0200

    Project with html css and js

---
+++ index.js
@@ -0,0 +1,22 @@
+const images = [
+    {
+        id:1,
+        title:"Web Application",
+        image:"/images/website-content 1.png"
+    },
+    {
+        id:2,
+        title:"SEO",
+        image:"/images/seo-tag 1.png"
+    },
+    {
+        id:3,
+        title:"AR/VR Solutions",
+        image:"/images/immersive 1.png"
+    },
+    {
+        id:4,
+        title:"Mobile Applications",
+        image:"/images/coding (1) 1.png"
+    }
+]
